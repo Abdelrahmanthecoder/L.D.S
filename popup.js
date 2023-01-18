0387ffa5471c525426c5056356b25b1de9568887
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-01-18 05:44:30 +0300

    popup.js updated

--- popup.js
+++ popup.js
@@ -31,27 +31,3 @@ if (document.querySelector(".popup")) {
     })
 
 }
-
-chrome.storage.local.set({ "key": "value" }, function() {
-    console.log("Value saved: " + value);
-});
-
-chrome.storage.local.get("key", function(items) {
-  console.log("Retrieved value: " + items.key);
-});
-
-chrome.storage.local.remove("key", function() {
-    console.log("Value removed");
-});
-
-chrome.storage.local.clear(function() {
-    console.log("All values removed");
-});  
-
-chrome.storage.local.set({ "key": "value" }, function() {
-    if (chrome.runtime.lastError) {
-      console.log(chrome.runtime.lastError);
-    } else {
-      console.log("Value saved: " + value);
-    }
-});
