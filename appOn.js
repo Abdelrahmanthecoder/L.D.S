6969b5842139e8135e1ff0ff72b325e80bceb8c6
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-02-13 17:59:07 +0300

    appOn.js updated

--- appOn.js
+++ appOn.js
@@ -2,6 +2,6 @@
     document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
     let media = document.querySelectorAll("img, picture, video");
     media.forEach((mediaItem) => {
-        mediaItem.style.filter = "invert(1) hue-rotate(190deg)"
+        mediaItem.style.filter = "invert(1.5) hue-rotate(190deg)"
     })
 })();
