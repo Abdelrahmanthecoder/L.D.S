fc8315c2302949deb75df5e4d0f9fa9bad3962c0
Abdelrahman B <99421749+Abdelrahmanthecoder@users.noreply.github.com>
2023-03-10 19:26:13 +0300

    appOn.js updated

--- appOn.js
+++ appOn.js
@@ -1,7 +1,5 @@
-(function () {
-    document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
-    let media = document.querySelectorAll("img, picture, video");
-    media.forEach((mediaItem) => {
-        mediaItem.style.filter = "invert(1.5) hue-rotate(190deg)"
-    })
-})();
+document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
+let media = document.querySelectorAll("img, picture, video");
+media.forEach((mediaItem) => {
+    mediaItem.style.filter = "invert(1.5) hue-rotate(190deg)";
+});
